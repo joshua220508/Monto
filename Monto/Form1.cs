524079f78c5ba1e1e8f71bc71b35d0887ff70b3a
joshua220508 <jesaenz@uamv.edu.ni>
2025-09-22 16:12:02 -0600

    Monto

---
+++ Monto/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Monto.Dao;
+
+namespace Monto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double precio = double.Parse(tbPrecio.Text);
+            lblMonto.Text = "Precio: " + precio + " Monto: " + Calculo.CalcularMonto(precio);
+
+        }
+
+
+        private void tbPrecio_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcularRef_Click(object sender, EventArgs e)
+        {
+            double precio = double.Parse(tbPrecio.Text);
+            lblMonto.Text =" Monto: " + Calculo.CalcularMonto(ref precio) + " Precio: " + precio ;
+
+        }
+    }
+}
